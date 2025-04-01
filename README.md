208612a3ad664efda980b99eb265d33ba5d21324
Victor Santos da Silva <santos.victors2000@gmail.com>
2025-03-31 21:51:30 -0300

    feat: add save database in mysql

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # WebScraping
+
+## Descrição
+
+Este projeto realiza a coleta de dados do site https://nahoradoocio.lowlevel.com.br/ e armazena as informações extraídas em um banco de dados MySQL utilizando o DBeaver para visualização e gerenciamento.
+
+## Tecnologias
+
+- Axios: Para fazer requisições HTTP e buscar os dados do site.
+- Cheerio: Para realizar o parsing e extração de dados HTML.
+- MySQL: Banco de dados relacional para armazenar as informações coletadas.
+- DBeaver: Ferramenta para gerenciamento do banco de dados MySQL.
+
+## Pré-requisitos
+
+Antes de começar, você precisa ter o seguinte instalado:
+
+- Node.js (com npm ou yarn)
+- MySQL ou XAMPP para rodar o MySQL localmente.
+- DBeaver para gerenciar o banco de dados.
+
+## Instalação
+
+#### Clone o repositório:
+
+```bash
+git clone https://github.com/victors21dev/WebScraping.git
+cd webscraping
+```
+
+#### Instale as dependências:
+
+```bash
+npm install
+```
+
+#### Ou, se você estiver usando yarn:
+
+```bash
+yarn install
+```
+
+#### Configure o banco de dados:
+
+- Crie um banco de dados no MySQL com o nome desejado (ex: scraping_db).
+- No DBeaver, crie uma conexão com o banco de dados MySQL e anote as credenciais de conexão (usuário, senha, host).
+
+#### Configure as variáveis de ambiente:
+
+Crie um arquivo .env na raiz do projeto com as seguintes variáveis:
+
+```plaintext
+DB_HOST=localhost
+DB_USER=seu_usuario
+DB_PASSWORD=sua_senha
+```
+
+## Executando o Projeto
+
+#### Para rodar o scraper, basta executar o script:
+
+```brash
+node index.js
+```
+
+Isso irá iniciar o processo de scraping, coletando os dados do site e inserindo-os no banco de dados MySQL.
+
+Depois de rodar o script, você pode visualizar os dados diretamente no DBeaver, conectando-se ao banco de dados com as credenciais configuradas.
+
+## Contribuindo
+
+- Faça o fork do repositório.
+- Crie uma nova branch (git checkout -b feature/nova-funcionalidade).
+- Faça suas alterações e commit com uma mensagem clara.
+- Push para a branch (git push origin feature/nova-funcionalidade).
+- Abra um pull request.
+
+## Testes
+
+Não foram definidos testes automáticos para este projeto até o momento, mas contribuições de testes são bem-vindas!
+
+## Licença
+
+Este projeto está licenciado sob a MIT License.
--- index.js
+++ index.js
@@ -1,17 +1,58 @@
+/* The code snippet `const axios = require("axios"); const cheerio = require("cheerio"); const mysql =
+require("mysql2"); require("dotenv").config();` is importing and setting up the necessary libraries
+and modules for the JavaScript script to perform web scraping and interact with a MySQL database. */
 const axios = require("axios");
 const cheerio = require("cheerio");
+const mysql = require("mysql2");
+require("dotenv").config();
 
+/* The code snippet `const connection = mysql.createConnection({ ... });` is establishing a connection
+to a MySQL database using the `mysql` library in Node.js. It creates a connection object
+`connection` with the specified configuration parameters: */
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+/* The code snippet `connection.connect(function (err) { ... });` is establishing a connection to a
+MySQL database using the `mysql` library in Node.js. */
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+/* The line `const URL = "https://nahoradoocio.lowlevel.com.br/";` is declaring a constant variable
+named `URL` and assigning it the value of the URL "https://nahoradoocio.lowlevel.com.br/". This URL
+is used as the target website from which the script will fetch data using Axios and then parse the
+HTML content using Cheerio. */
 const URL = "https://nahoradoocio.lowlevel.com.br/";
 
 async function main() {
+  /* This JavaScript code is a web scraping script that fetches data from a specific website
+(https://nahoradoocio.lowlevel.com.br/) using Axios to make an HTTP request and Cheerio to parse the
+HTML content of the response. */
   const response = await axios(URL);
   const html = response.data;
   const $ = cheerio.load(html);
+
+  /* The lines `const YouMayLike = [];` and `const GridBlog = [];` are initializing two empty arrays
+named `YouMayLike` and `GridBlog` respectively. These arrays are used to store the extracted data
+from the web scraping process for the "You may like" section and the "GridBlog" section of the
+target website. */
   const YouMayLike = [];
   const GridBlog = [];
 
   //   You may like
   $(".related-posts section").each(function () {
+    /* The code snippet you provided is part of a web scraping script written in JavaScript using Axios
+ and Cheerio libraries. In this specific part of the script, the code is targeting elements within
+ the HTML structure of the fetched webpage to extract specific information related to posts in the
+ "You may like" section. */
     const title = $(this).find("h4 a").html();
     const url = $(this).find("a").attr("href");
     const img_gif_src = $(this).find("a img").attr("src");
@@ -27,13 +68,22 @@ async function main() {
 
   //   GridBlog
   $(".blog-grid li").each(function () {
+    /* The provided code snippet is part of a web scraping script written in JavaScript using Axios and
+Cheerio libraries. In this specific part of the script, the code is targeting elements within the
+HTML structure of the fetched webpage to extract specific information related to posts in the
+"GridBlog" section. */
     const title = $(this).find("article .post-header .post-title a").html();
     const category = $(this)
       .find("article .post-header .post-categories a")
       .html();
-    const comments = $(this)
+    let comments = $(this)
       .find("article .post-header .post-meta .post-comments")
       .html();
+    if (comments.length < 14) {
+      comments = Number(comments.slice(0, 1));
+    } else {
+      comments = Number(comments.slice(0, 2));
+    }
     const url = $(this).find("article .post-header .post-title a").attr("href");
     const img_gif_src = $(this).find("li article .post-media img").attr("src");
     //
@@ -46,8 +96,25 @@ async function main() {
     });
   });
 
-  console.log(YouMayLike);
-  console.log(GridBlog);
+  /* The code snippet you provided is iterating over each element in the `YouMayLike` array using the
+`forEach` method. For each element in the array, it is executing a SQL query to insert data into a
+MySQL database table named `you_may_like`. */
+  YouMayLike.forEach((element) => {
+    connection.query(
+      `INSERT INTO you_may_like(title, url, img_gif_src, publication_date) VALUES ('${element.title}', '${element.url}','${element.img_gif_src}','${element.publication_date}')`,
+      function (err, results) {}
+    );
+  });
+
+  /* The code snippet you provided is iterating over each element in the `GridBlog` array using the
+`forEach` method. For each element in the array, it is executing a SQL query to insert data into a
+MySQL database table named `you_may_like`. */
+  GridBlog.forEach((element) => {
+    connection.query(
+      `INSERT INTO gridblog(title, category, comments, url, img_gif_src) VALUES ('${element.title}', '${element.category}','${element.comments}','${element.url}', '${element.img_gif_src}')`,
+      function (err, results) {}
+    );
+  });
 }
 
 main();
